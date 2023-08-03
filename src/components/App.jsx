c25fadb3251dc88d14a7f6225dbf74b0d9ba0521
SKhrebtan <shrebtan@gmail.com>
2023-08-03 20:45:53 +0300

    refactoring - App is main logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,115 @@
 import React, { Component } from 'react';
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
-import css from '../Styles.module.css'
+
+import Button from './Button';
+import Loader from './Loader';
+import waitImg from '../images/waiting.jpg';
+import errorImg from '../images/error.jpg';
+import emptyImg from '../images/empty.jpg';
+import { GetImages } from './FetchImages';
+import css from 'Styles.module.css'
 import { ToastContainer } from 'react-toastify';
 import PropTypes from 'prop-types';
 
 class App extends Component {
   state = {
     searchValue: '',
-       }
+    page: 1,
+    totalPages: 0,
+    result: [],
+    status: 'idle',
+    error: null,
+  }
+
+  messagesEndRef = React.createRef();
+
+   static propTypes = {
+    page: PropTypes.number,
+    totalPages: PropTypes.number,
+    result: PropTypes.array,
+    status: PropTypes.string,
+    value: PropTypes.string,
+    error: PropTypes.string,
+  };
+ 
+     componentDidUpdate(prevProps, prevState) {
+        const { page, searchValue } = this.state;
+       
+    if (prevState.searchValue !== searchValue || prevState.page !== page) {
+      this.setState({ status: 'pending' });
+     
+      GetImages(searchValue, page)
+            .then(response => {
+              if (response.ok) {
+                return response.json();
+              }
+              return Promise.reject(
+                new Error('Щось пішло не так, повторіть спробу')
+              );
+            })
+            .then(({hits, totalHits}) => {
+              this.setState({
+                result:
+                  page === 1
+                    ? hits
+                    : [...prevState.result, ...hits],
+                status: totalHits === 0 ? 'empty' : 'resolved',
+                totalPages: Math.floor(totalHits / 12),
+              });
+            })
+                  .catch(error => this.setState({ error, status: 'rejected' }))
+       }  
+       this.scrollToBottom(); 
+  }
+
+  onLoadMore = () => {
+    this.setState(({ page }) => ({ page: page + 1 }));
+       };
+
   
   handleFormSubmit = (searchValue) => {
-    this.setState({ searchValue })
-      }
+     this.setState({ searchValue, page: this.state.page !== 1 ? 1 : this.state.page })
+  }
+  
+        scrollToBottom = () => {
+    this.messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' })
+  }
     
   render() {
-       const { searchValue } = this.state;
-    
-    return (<div className={css.app}>
+       const { status, page, totalPages, result, error } = this.state;
+  
+    return (
+      <div className={css.app}>
       <ToastContainer autoClose={2000} />
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        {(status === 'idle') && (<h1 className={css.idletitle}>Введіть запит</h1>)}
+        <ImageGallery result={result} />  
+       {(status === 'pending') && (
+        <>
+          <Loader />
+          <img src={waitImg} alt="await" width="480" className={css.infoImage} />
+        </>
+      )
+        }  
+          { (status === 'rejected') && (
+          <div> <h1 className={css.idletitle}>{error.message}</h1>
+          <img src={errorImg} alt="error" width="480" className={css.infoImage} /></div>
+         
+      )
+        }
+         { status === 'empty' && (
+      <div>
+         <h1 className={css.idletitle}>Нема результатів по даному запиту</h1>
+          <img src={emptyImg} alt="empty" width="480" className={css.infoImage} />   
+      </div>   
+              )
+    }
     
-      <Searchbar onSubmit={this.handleFormSubmit} />
-      
-      <ImageGallery value={searchValue} />
-      
+       {result.length > 0 && page <= totalPages && (
+            <Button loadMore={this.onLoadMore} />
+        )}
+             < div ref = { this.messagesEndRef } />  
    </div>
      
   );
@@ -36,3 +122,19 @@ export default App;
 App.propTypes = {
   searchValue: PropTypes.string,
 }
+
+
+ 
+      
+    
+      
+    
+
+
+
+    
+  
+ 
+
+  
+   
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,85 +1,23 @@
 import React, { Component } from 'react';
 import ImageGalleryItem from '../ImageGalleryItem';
 import Modal from '../Modal'
-import Button from '../Button';
-import Loader from '../Loader';
-import {GetImages} from '../FetchImages'
 import css from '../../Styles.module.css';
-import waitImg from '../../images/waiting.jpg';
-import errorImg from '../../images/error.jpg';
-import emptyImg from '../../images/empty.jpg';
 import PropTypes from 'prop-types';
     
 export default class ImageGallery extends Component {
   state = {
-    page: 1,
-    totalPages: 0,
-    result: null,
-    status: 'idle',
-    value: '',
-    error: null,
-    showModal: false,
-    modalData: {
+  showModal: false,
+   modalData: {
       largeImageURL: 'https://ik.imagekit.io/demo/medium_cafe_B1iTdD0C.jpg',
       tags: '',
     },
   };
   
   static propTypes = {
-    page: PropTypes.number,
-    totalPages: PropTypes.number,
-    result: PropTypes.array,
-    status: PropTypes.string,
-    value: PropTypes.string,
-    error: PropTypes.string,
     showModal: PropTypes.bool,
     modalData: PropTypes.object
   };
-  messagesEndRef = React.createRef();
-    
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (prevState.value !== nextProps.value) {
-      return { page: 1, value: nextProps.value };
-    }
-    return null;
-  }
-
-    componentDidUpdate(prevProps, prevState) {
-        const { page } = this.state;
-        const currentValue = this.props.value
-
-    if (prevProps.value !== currentValue || prevState.page !== page
-    ) {
-      this.setState({ status: 'pending' });
-     
-      GetImages(currentValue, page)
-            .then(response => {
-              if (response.ok) {
-                return response.json();
-              }
-              return Promise.reject(
-                new Error('Щось пішло не так, повторіть спробу')
-              );
-            })
-            .then(({hits, totalHits}) => {
-              this.setState({
-                result:
-                  page === 1
-                    ? hits
-                    : [...prevState.result, ...hits],
-                status: 'resolved',
-                totalPages: Math.floor(totalHits / 12),
-              });
-            })
-                  .catch(error => this.setState({ error, status: 'rejected' }))
-      }
-      this.scrollToBottom()
-  }
-
-  onLoadMore = () => {
-    this.setState(({ page }) => ({ page: page + 1 }));
-       };
-
+  
   toggleModal = () => {
     this.setState(({ showModal }) => ({
       showModal: !showModal,
@@ -90,46 +28,12 @@ export default class ImageGallery extends Component {
     this.setState({ modalData, showModal: true });
   };
   
-   scrollToBottom = () => {
-    this.messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' })
-  }
+
 
   render() {
-    const { result, status, showModal, modalData, error, page, totalPages } = this.state;
-    console.log(this.state.totalPages);
-    console.log(this.state.page);
-          if (status === 'idle') {
-            return <h1 className={css.idletitle}>Введіть запит</h1>;
-          }
-      
-    if (status === 'pending') {
-      return (
-        <>
-          <Loader />
-          <img src={waitImg} alt="await" width="480" className={css.infoImage} />
-        </>
-      );
-    }
-      
-    if (status === 'rejected') {
-      return (
-        <>
-          <h1 className={css.idletitle}>{error.message}</h1>
-          <img src={errorImg} alt="error" width="480" className={css.infoImage} />
-        </>
-      );
-    }
-
-    if (result === null || result.length === 0) {
-      return (
-        <>
-          <h1 className={css.idletitle}>Нема результатів по даному запиту</h1>
-          <img src={emptyImg} alt="empty" width="480" className={css.infoImage} />
-        </>
-      );
-    }
-
-    if (status === 'resolved') {
+    const { showModal, modalData} = this.state;
+     const { result } = this.props;
+       
       return (
         <>
           <ul className={css.gallery}>
@@ -142,15 +46,10 @@ export default class ImageGallery extends Component {
                 />
               ))}
           </ul>
-          {result.length > 0 && page <= totalPages && (
-            <Button loadMore={this.onLoadMore} />
-          )}
-          {showModal && (
-            <Modal modalData={modalData} onClose={this.toggleModal} />
-          )}
-          <div ref={this.messagesEndRef} />
+         
+           {showModal && (
+            <Modal modalData={modalData} onClose={this.toggleModal} />)}
         </>
       );
     }
   }
-}
